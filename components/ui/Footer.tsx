6f3297828395417070dfe22dfc300715d2310cfa
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-07-28 19:58:10 +0530

    feat: added medium social link

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -6,6 +6,7 @@ export default function Footer() {
     GITHUB: 'https://github.com/blueorionn',
     X: 'https://x.com/SSwadhinTandi',
     BLUESKY: 'https://bsky.app/profile/sswadhinn.bsky.social',
+    MEDIUM: 'https://medium.com/@blueorionn',
     MAIL: 'swadhintandi@yahoo.com',
   }
 
@@ -27,6 +28,13 @@ export default function Footer() {
         <Link href={`${SOCIAL_LINKS.X}`} target='_blank'>
           <AppIcon
             name='brand-x'
+            className='text-slate-400 transition-colors duration-300 hover:text-cyan-600'
+          />
+        </Link>
+        <Link href={`${SOCIAL_LINKS.MEDIUM}`} target='_blank'>
+          <AppIcon
+            type='grommet-icons'
+            name='medium'
             className='text-slate-400 transition-colors duration-300 hover:text-cyan-600'
           />
         </Link>
